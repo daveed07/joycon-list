5390f808262cee307e57e1370e2eff1612cbbe70
David Zuñiga <davidz0703@gmail.com>
2022-01-17 12:13:52 -0500

    Identifier added to registers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const writeData = require("./fileWriter.js");
+const idGen = require("./fileReader.js");
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const moment = require("moment");
+const fs = require("fs");
 const app = express();
 const port = 8080;
 
@@ -13,16 +15,27 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/send", (req, res) => {
   const body = req.body;
-  const data = {
-    date: `${moment().subtract(10, "days").calendar()} ${moment().format(
-      "LT"
-    )}`,
+  const date = `${moment().subtract(10, "days").calendar()} ${moment().format(
+    "LT"
+  )}`
+  fs.readFile("./src/log.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    let arr = JSON.parse(data);
+    let id = arr.users.length + 1;
+    const info = {
+    date: date,
+    id: idGen(id),
     name: body.name,
     phone: body.phone,
     select: body.select,
     color: body.color,
   };
-  writeData(data);
+  writeData(info);
+  });
+
+  const idGen = (id) => {
+    return id;
+  };
   res.json({ message: "I got the data" });
 });
 
